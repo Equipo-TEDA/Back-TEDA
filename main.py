02ae61943efb15c4fadef667a4b667757e8950af
ASGuzman <aylusolguzmanhoyos@gmail.com>
2024-08-07 16:23:38 -0300

    Carga endpoints de la pagina 2

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routers.page1 import router_1
 from routers.general_filters import router_filters
+from routers.page2 import router_3
 
 app = FastAPI()
 
 app.include_router(router_filters)
 app.include_router(router_1)
+app.include_router(router_3)
 
 @app.get("/")
 async def root():
